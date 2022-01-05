5997b204022b381a18587c0b2cf9ca8b17ab52fd
Francesko Racaku <franceskoracaku@hotmail.com>
2022-01-05 17:51:03 -0500

    Fixes

--- index.js
+++ index.js
@@ -1,14 +1,134 @@
 //import * as express from "express";
+
 const express = require("express");
 const querystring = require("querystring");
 var session = require('express-session');
+const AWS = require('aws-sdk');
 const { process_params } = require("express/lib/router");
 const { URLSearchParams } = require("url");
 const app = express();
-const port = process.env.PORT || 3400;
+const port = process.env.PORT || 8000;
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+AWS.config.loadFromPath('./credentials.json');
+
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+app.post("/create", (req, res) =>{
+    let body = req.body;
+    console.log(body);
+    let input = {
+        "email": body.email,
+        "password": body.password,
+        
+    };
+    let params = {
+        TableName: body.TableName,
+        Item: input
+    };
+    docClient.put(params, function(err, data){
+
+        if(err){
+            console.log("users::save::error - " + JSON.stringify(err, null, 2));
+            res.status(404).send("Oops! User is not saved in dynamoDB")
+
+        } else {
+            console.log("users::save::success");
+            res.status(200).send("Great! User saved in dynamoDB")
+        }
+    });
+
+});
+
+app.get("/read", async (req,res) =>{
+    let params ={
+        TableName: "new_data"
+    }
+
+    //const data = await docClient.scan(params).promise();
+    const data = await docClient.scan(params).promise().catch(err =>{
+        console.log(err);
+        res.status(400).send(err);
+    });
+    console.log(data);
+    res.status(200).send(data);
+})
+
+app.get("/read/:id", (req, res) =>{
+    let body = req.body;
+
+    let params = {
+        TableName: "new_data",
+        Key: {
+            "email": req.params.id
+        }
+    };
+    docClient.get(params, function(err, data){
+
+        if(err){
+            console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+            res.status(404).send("No Values Found")
+
+        } else {
+            console.log("users::fetchOneByKey::success - "+ JSON.stringify(data, null, 2));
+            res.status(200).send(data)
+        }
+    });
+
+});
+
+app.post("/update", (req, res) =>{
+    const body = req.body;
+
+    let params = {
+        TableName: body.TableName,
+        Key: { "email": body.email },
+        UpdateExpression: "set password = :byPassword",
+        ExpressionAttributeValues:{
+            ":byPassword": body.password,
+        },
+
+        ReturnValues: "UPDATED_NEW"
+    };
+    docClient.update(params, function(err, data){
+
+        if(err){
+            console.log("users::update::error - " + JSON.stringify(err, null, 2));
+            res.status(404).send("Oops! Not Updated")
+
+        } else {
+            console.log("users::update::success" + JSON.stringify(data));
+            res.status(200).send(data)
+        }
+    });
+
+});
+
+
+app.delete("/delete", (req,res)=>{
+    let params = {
+        TableName: "new_data",
+        Key: {
+            "email": body.email,
+
+        }
+    };
+
+    docClient.delete(params, function(err, data){
+        if(err){
+            console.log("users::save::error - " + JSON.stringify(err, null, 2));
+            res.status(404).send("Oops! User is not deleted")
+
+        } else {
+            console.log("users::save::success");
+            res.status(200).send("Great! User is deleted")
+        }
+    });
+
+});
+
 
 app.use(session({
     secret :"sdasd",
@@ -67,7 +187,7 @@ app.post("/signup", (req,res)=> {
     req.session.destroy(() => {
         console.log("user reset");
     })
-    res.redirect("/?reason=invalid_user");
+    res.redirect("/signout");
 }
 
 });
@@ -86,13 +206,49 @@ app.get("/main", (req,res) => {
 });
 
 
-app.get("/warmup", (req,res)=>{
+const auth = (req,res,next) =>{
     if (req.session && req.session.username) {
+    console.log("this is the first pistop");
+    next();
+    }else {
+        res.redirect("/?reason=invalid_login");
+    }
+}
+
+const mw = (req,res, next)=>{
+    console.log("first mw")
+    next();
+}
+const m2 = (req,res, next)=>{
+    console.log("second mw")
+    next();
+}
+const middlewares = [mw, m2,auth];
+
+app.get("/*", middlewares);
+
+
+app.get("/signout", (req,res)=> {
+
+    req.session.destroy(() => {
+        res.render("signout");
+    });
+
+});
+
+// const pitstop2 = (req,res,next) =>{
+//     res.end("end here");
+//     console.log("this is the second pistop");
+//     next();
+// 
+app.get("/warmup", (req,res)=>{
+    // if (req.session && req.session.username) {
     user = req.session.username;
     res.render("warm", {my_user: user});
-} else{
-    res.redirect("/");
-}
+    location = "warm";
+/* } else{
+    // res.redirect("/");
+}*/
 });
 
 app.get("/round1", (req,res)=>{
@@ -158,6 +314,13 @@ app.get("/round1/yourtheboss/win", (req,res)=>{
     res.redirect("/");
 }
 });
+
+app.get("/signup", (req,res)=> {
+
+    res.render("signup", { title: 'Signup Page'}); 
+    
+ }); 
+
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
